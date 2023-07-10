7ca072748661189daefd7b0d5959a922cb181200
Eduardo Herrera <eduardoherrera@educodev.com>
2023-07-10 22:58:54 +0200

    Initial Commit

---
+++ Rick&Morty app/Rick_Morty_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_Morty_appApp.swift
+//  Rick&Morty app
+//
+//  Created by Eduardo Herrera on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_Morty_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
